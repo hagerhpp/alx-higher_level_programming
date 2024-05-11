cbb8be52f2e3e6f706ba9786218e41c15c157648
root <you@example.com>
2024-05-11 10:50:33 -0700

    0x00-python-hello_world

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+str = "The Zen of Python, by Tim Peters.\n
+\n
+Beautiful is better than ugly.\n
+Explicit is better than implicit.\n
+Simple is better than complex.\n
+Complex is better than complicated.\n
+Flat is better than nested.\n
+Sparse is better than dense.\n
+Readability counts.\n "
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
